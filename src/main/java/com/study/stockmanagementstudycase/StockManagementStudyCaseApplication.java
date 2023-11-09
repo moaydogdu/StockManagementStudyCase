bc3ba25fb7255462f988025e2056fffb7c3fa11c
Muhammet Oguzhan AYDOGDU <m.o.aydogdu@outlook.com>
2023-11-10 00:32:26 +0300

    Initial Commit

---
+++ src/main/java/com/study/stockmanagementstudycase/StockManagementStudyCaseApplication.java
@@ -0,0 +1,13 @@
+package com.study.stockmanagementstudycase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagementStudyCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockManagementStudyCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/stockmanagementstudycase/StockManagementStudyCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.stockmanagementstudycase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagementStudyCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
