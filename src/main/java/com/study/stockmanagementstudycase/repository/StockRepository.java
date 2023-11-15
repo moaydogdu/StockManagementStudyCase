7810d2b3e33f0e7cebb83af961e2b9417e42b458
Muhammet Oguzhan AYDOGDU <m.o.aydogdu@outlook.com>
2023-11-15 22:44:43 +0300

    Refactor StockRepository bugFix

--- src/main/java/com/study/stockmanagementstudycase/repository/StockRepository.java
+++ src/main/java/com/study/stockmanagementstudycase/repository/StockRepository.java
@@ -1,8 +1,9 @@
 package com.study.stockmanagementstudycase.repository;
 
+import com.study.stockmanagementstudycase.model.entities.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StockRepository extends JpaRepository {
+public interface StockRepository extends JpaRepository<Stock, String> {
 }
