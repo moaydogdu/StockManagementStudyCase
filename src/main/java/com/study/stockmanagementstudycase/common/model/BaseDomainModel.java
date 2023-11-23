29ebbdfc2952e127d470485e8f8d3265c6e74e5d
moayd <m.o.aydogdu@outlook.com>
2023-11-24 00:55:54 +0300

    Create BaseDomainModel.java

---
+++ src/main/java/com/study/stockmanagementstudycase/common/model/BaseDomainModel.java
@@ -0,0 +1,21 @@
+package com.study.stockmanagementstudycase.common.model;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class BaseDomainModel {
+    public LocalDateTime createdAt;
+    public LocalDateTime updatedAt;
+}
