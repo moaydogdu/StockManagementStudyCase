04f3641a57e4c892d0ea90f28be5dc058394d0d8
moayd <m.o.aydogdu@outlook.com>
2023-11-24 01:16:07 +0300

    Delete WareHouseUpdateService.java

--- src/main/java/com/study/stockmanagementstudycase/service/WareHouseUpdateService.java
+++
@@ -1,7 +0,0 @@
-package com.study.stockmanagementstudycase.service;
-
-import com.study.stockmanagementstudycase.model.dto.request.WareHouseUpdateRequest;
-
-public interface WareHouseUpdateService {
-    void updateWareHouse(final WareHouseUpdateRequest updateRequest,final String id);
-}
