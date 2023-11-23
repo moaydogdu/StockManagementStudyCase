12ebe280f55b8c645a8af11d0101d8a73fa6dc97
moayd <m.o.aydogdu@outlook.com>
2023-11-24 01:11:45 +0300

    Refactor WareHouseResponse.java

--- src/main/java/com/study/stockmanagementstudycase/model/dto/response/WareHouseResponse.java
+++ src/main/java/com/study/stockmanagementstudycase/model/dto/response/WareHouseResponse.java
@@ -1,6 +1,10 @@
 package com.study.stockmanagementstudycase.model.dto.response;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Builder
 @Getter
