cd2a1c985bfcb620b9111cfb770f4db468fec7e7
moayd <m.o.aydogdu@outlook.com>
2023-11-24 01:16:26 +0300

    Refactor WareHouseUpdateServiceImpl.java

--- src/main/java/com/study/stockmanagementstudycase/service/wareHouse/impl/WareHouseUpdateServiceImpl.java
+++ src/main/java/com/study/stockmanagementstudycase/service/wareHouse/impl/WareHouseUpdateServiceImpl.java
@@ -1,14 +1,12 @@
-package com.study.stockmanagementstudycase.service.impl;
+package com.study.stockmanagementstudycase.service.wareHouse.impl;
 
-import com.study.stockmanagementstudycase.model.dto.request.WareHouseUpdateRequest;
+import com.study.stockmanagementstudycase.model.dto.request.wareHouse.WareHouseUpdateRequest;
 import com.study.stockmanagementstudycase.model.entities.WareHouseEntity;
-import com.study.stockmanagementstudycase.model.mappers.WareHouseMapper;
+import com.study.stockmanagementstudycase.model.mappers.wareHouse.WareHouseMapper;
 import com.study.stockmanagementstudycase.repository.WareHouseRepository;
-import com.study.stockmanagementstudycase.service.WareHouseUpdateService;
+import com.study.stockmanagementstudycase.service.wareHouse.WareHouseUpdateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
