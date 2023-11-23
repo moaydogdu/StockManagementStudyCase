a74666fbb1d82d9b9dd3a77537540c5cdcbbdda0
moayd <m.o.aydogdu@outlook.com>
2023-11-24 01:00:12 +0300

    Create StockMapper.java

---
+++ src/main/java/com/study/stockmanagementstudycase/model/mappers/stock/StockMapper.java
@@ -0,0 +1,49 @@
+package com.study.stockmanagementstudycase.model.mappers.stock;
+
+import com.study.stockmanagementstudycase.model.dto.Stock;
+import com.study.stockmanagementstudycase.model.dto.request.stock.StockCreateRequest;
+import com.study.stockmanagementstudycase.model.entities.StockEntity;
+
+import java.math.BigDecimal;
+
+public class StockMapper {
+
+    public static StockEntity mapForCreating(
+            final StockCreateRequest stockCreateRequest
+    ) {
+        return StockEntity.builder()
+                .name(stockCreateRequest.getName())
+                .price(stockCreateRequest.getPrice())
+                .amount(BigDecimal.ZERO)
+                .unitType(stockCreateRequest.getUnitType())
+                .build();
+    }
+
+    public static Stock toDomainModel(
+            final StockEntity stockEntity
+    ) {
+        return Stock.builder()
+                .id(stockEntity.getId())
+                .name(stockEntity.getName())
+                .price(stockEntity.getPrice())
+                .amount(stockEntity.getAmount())
+                .unitType(stockEntity.getUnitType())
+                .createdAt(stockEntity.getCreatedAt())
+                .updatedAt(stockEntity.getUpdatedAt())
+                .build();
+    }
+
+    public static StockEntity toEntity(
+            final Stock stockDomainModel
+    ) {
+        return StockEntity.builder()
+                .id(stockDomainModel.getId())
+                .name(stockDomainModel.getName())
+                .price(stockDomainModel.getPrice())
+                .amount(stockDomainModel.getAmount())
+                .unitType(stockDomainModel.getUnitType())
+                .createdAt(stockDomainModel.getCreatedAt())
+                .updatedAt(stockDomainModel.getUpdatedAt())
+                .build();
+    }
+}
