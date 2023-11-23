0885260c0f6584c3a399dd2911d5631c35566857
moayd <m.o.aydogdu@outlook.com>
2023-11-24 01:14:33 +0300

    Create WareHouseStockUpdateService.java

---
+++ src/main/java/com/study/stockmanagementstudycase/service/wareHouseStock/WareHouseStockUpdateService.java
@@ -0,0 +1,17 @@
+package com.study.stockmanagementstudycase.service.wareHouseStock;
+
+import com.study.stockmanagementstudycase.model.WareHouse;
+import com.study.stockmanagementstudycase.model.WareHouseStock;
+import com.study.stockmanagementstudycase.model.dto.Stock;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public interface WareHouseStockUpdateService {
+    WareHouseStock updateWareHouseStockForStockEntry(
+            final Stock stock,
+            final WareHouse wareHouse,
+            final BigDecimal entryAmount,
+            final LocalDateTime entryTime
+    );
+}
