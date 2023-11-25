09839eacc807d1fca63d69dd54106f5e502bb9e1
moayd <m.o.aydogdu@outlook.com>
2023-11-26 00:29:02 +0300

    Refactor return type of WareHouseCreateService.java

--- src/main/java/com/study/stockmanagementstudycase/service/wareHouse/WareHouseCreateService.java
+++ src/main/java/com/study/stockmanagementstudycase/service/wareHouse/WareHouseCreateService.java
@@ -1,9 +1,10 @@
 package com.study.stockmanagementstudycase.service.wareHouse;
 
+import com.study.stockmanagementstudycase.model.WareHouse;
 import com.study.stockmanagementstudycase.model.dto.request.wareHouse.WareHouseCreateRequest;
 
 public interface WareHouseCreateService {
-    void createWareHouse(
+    WareHouse createWareHouse(
             final WareHouseCreateRequest request
     );
 }
