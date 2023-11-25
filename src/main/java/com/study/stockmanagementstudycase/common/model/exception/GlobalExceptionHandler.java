349ef87dacffd04ba2bcac13b06413a3c8061fba
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2023-11-26 00:25:31 +0100

    The class name was changed from GeneralException Advisor to Global Exception Handler and Refactor was done

--- src/main/java/com/study/stockmanagementstudycase/common/model/exception/GlobalExceptionHandler.java
+++ src/main/java/com/study/stockmanagementstudycase/common/model/exception/GlobalExceptionHandler.java
@@ -1,23 +1,20 @@
-package com.study.stockmanagementstudycase.exception;
+package com.study.stockmanagementstudycase.common.model.exception;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class GeneralExceptionAdvisor extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach(
                 error -> {
