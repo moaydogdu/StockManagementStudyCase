a8a21c4afee379fef4c1dd3373d83a85f606e0a8
moayd <m.o.aydogdu@outlook.com>
2023-12-06 19:54:04 +0300

    Create WareHouseCreateRequestBuilder.java by @mehmetseymusyuzen

---
+++ src/test/java/com/study/stockmanagementstudycase/builder/dto/WareHouseCreateRequestBuilder.java
@@ -0,0 +1,42 @@
+package com.study.stockmanagementstudycase.builder.dto;
+
+import com.github.javafaker.Faker;
+import com.study.stockmanagementstudycase.builder.BaseBuilder;
+import com.study.stockmanagementstudycase.model.dto.request.wareHouse.WareHouseCreateRequest;
+
+public class WareHouseCreateRequestBuilder extends BaseBuilder<WareHouseCreateRequest> {
+
+    public WareHouseCreateRequestBuilder(
+    ) {
+        super(WareHouseCreateRequest.class);
+    }
+
+    public WareHouseCreateRequestBuilder withValidFields(
+    ) {
+        Faker javaFaker = new Faker();
+        return this
+                .withName(javaFaker.name().toString())
+                .withAddress(javaFaker.address().fullAddress());
+    }
+
+    public WareHouseCreateRequestBuilder withName(
+            final String name
+    ) {
+        data.setName(name);
+        return this;
+    }
+
+    public WareHouseCreateRequestBuilder withAddress(
+            final String address
+    ) {
+        data.setAddress(address);
+        return this;
+    }
+
+    @Override
+    public WareHouseCreateRequest build(
+    ) {
+        return super.build();
+    }
+
+}
