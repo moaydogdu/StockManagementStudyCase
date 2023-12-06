62b41c9cb7aa25e085b9a59ac1d4a595a15fb246
moayd <m.o.aydogdu@outlook.com>
2023-12-06 19:51:21 +0300

    Create WareHouseUpdateServiceImplTest.java

---
+++ src/test/java/com/study/stockmanagementstudycase/service/warehouse/impl/WareHouseUpdateServiceImplTest.java
@@ -0,0 +1,158 @@
+package com.study.stockmanagementstudycase.service.warehouse.impl;
+
+import com.study.stockmanagementstudycase.base.BaseServiceTest;
+import com.study.stockmanagementstudycase.builder.dto.WareHouseUpdateRequestBuilder;
+import com.study.stockmanagementstudycase.common.exception.WareHouseAlreadyExistException;
+import com.study.stockmanagementstudycase.common.exception.WareHouseNotFoundException;
+import com.study.stockmanagementstudycase.model.WareHouse;
+import com.study.stockmanagementstudycase.model.dto.request.wareHouse.WareHouseUpdateRequest;
+import com.study.stockmanagementstudycase.model.entities.WareHouseEntity;
+import com.study.stockmanagementstudycase.repository.WareHouseRepository;
+import com.study.stockmanagementstudycase.service.wareHouse.impl.WareHouseUpdateServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.Optional;
+
+public class WareHouseUpdateServiceImplTest extends BaseServiceTest {
+
+    @InjectMocks
+    private WareHouseUpdateServiceImpl wareHouseUpdateService;
+
+    @Mock
+    private WareHouseRepository wareHouseRepository;
+
+    @Test
+    void givenValidWareHouseUpdateRequest_whenUpdateWareHouse_thenUpdateWareHouse() {
+        // Given
+        WareHouse wareHouse = new WareHouse();
+        String wareHouseId = wareHouse.getId();
+
+        final WareHouseUpdateRequest validWareHouseUpdateRequest = new WareHouseUpdateRequestBuilder()
+                .withValidFields()
+                .build();
+
+        // When
+        Mockito.when(
+                wareHouseRepository.findById(
+                        wareHouseId)
+        ).thenReturn(Optional.of(new WareHouseEntity()));
+
+        Mockito.when(
+                wareHouseRepository.existsWareHouseEntitiesByNameAndAddress(
+                        Mockito.anyString(),
+                        Mockito.anyString()
+                )
+        ).thenReturn(Boolean.FALSE);
+
+        // Then
+        Assertions.assertDoesNotThrow(
+                () -> wareHouseUpdateService.updateWareHouse(
+                        validWareHouseUpdateRequest,
+                        wareHouseId
+                )
+        );
+
+        // Verify
+        Mockito.verify(
+                wareHouseRepository,
+                Mockito.times(1)
+        ).findById(wareHouseId);
+
+        Mockito.verify(
+                wareHouseRepository,
+                Mockito.times(1)
+        ).existsWareHouseEntitiesByNameAndAddress(Mockito.anyString(), Mockito.anyString());
+
+        Mockito.verify(
+                wareHouseRepository,
+                Mockito.times(1)
+        ).save(Mockito.any(WareHouseEntity.class));
+    }
+
+    @Test
+    void givenExistWareHouseUpdateRequest_whenUpdateWareHouse_thenThrowsWareHouseAlreadyExistException() {
+        // Given
+        final WareHouseUpdateRequest notValidWareHouseUpdateRequest = new WareHouseUpdateRequestBuilder()
+                .withValidFields()
+                .build();
+
+        // When
+        Mockito.when(wareHouseRepository.existsWareHouseEntitiesByNameAndAddress(
+                notValidWareHouseUpdateRequest.getName(),
+                notValidWareHouseUpdateRequest.getAddress()
+        )).thenReturn(Boolean.TRUE);
+
+        // Then
+        Assertions.assertThrows(
+                WareHouseAlreadyExistException.class,
+                () -> wareHouseUpdateService.updateWareHouse(
+                        notValidWareHouseUpdateRequest,
+                        Mockito.anyString())
+        );
+
+        // Verify
+        Mockito.verify(
+                wareHouseRepository,
+                Mockito.times(1)
+        ).existsWareHouseEntitiesByNameAndAddress(Mockito.anyString(), Mockito.anyString());
+
+        Mockito.verify(
+                wareHouseRepository,
+                Mockito.times(0)
+        ).findById(Mockito.anyString());
+
+        Mockito.verify(
+                wareHouseRepository,
+                Mockito.times(0)
+        ).save(Mockito.any(WareHouseEntity.class));
+    }
+
+    @Test
+    void givenEmptyWareHouseEntity_whenUpdateWareHouse_thenThrowsWareHouseNotFoundException() {
+        // Given
+        final WareHouseUpdateRequest validWareHouseUpdateRequest = new WareHouseUpdateRequestBuilder()
+                .withValidFields()
+                .build();
+
+        // When
+        Mockito.when(wareHouseRepository.existsWareHouseEntitiesByNameAndAddress(
+                validWareHouseUpdateRequest.getName(),
+                validWareHouseUpdateRequest.getAddress()
+        )).thenReturn(Boolean.FALSE);
+
+        Mockito.when(wareHouseRepository.findById(Mockito.anyString()))
+                .thenReturn(Optional.empty());
+
+        // Then
+        Assertions.assertThrows(
+                WareHouseNotFoundException.class,
+                () -> wareHouseUpdateService.updateWareHouse(
+                        validWareHouseUpdateRequest,
+                        Mockito.anyString()
+                )
+        );
+
+        // Verify
+        Mockito.verify(
+                wareHouseRepository,
+                Mockito.times(1)
+        ).findById(Mockito.anyString());
+
+        Mockito.verify(
+                wareHouseRepository,
+                Mockito.times(1)
+        ).existsWareHouseEntitiesByNameAndAddress(
+                validWareHouseUpdateRequest.getName(),
+                validWareHouseUpdateRequest.getAddress());
+
+        Mockito.verify(
+                wareHouseRepository,
+                Mockito.times(0)
+        ).save(Mockito.any(WareHouseEntity.class));
+    }
+
+}
