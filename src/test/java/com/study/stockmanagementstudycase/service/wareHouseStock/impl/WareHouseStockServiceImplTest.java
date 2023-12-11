fce864f64a53c888a7ebbe94a74a80f5fb8bbdae
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2023-12-11 22:10:13 +0100

    Create WareHouseStockServiceImplTest.java

---
+++ src/test/java/com/study/stockmanagementstudycase/service/wareHouseStock/impl/WareHouseStockServiceImplTest.java
@@ -0,0 +1,157 @@
+package com.study.stockmanagementstudycase.service.wareHouseStock.impl;
+
+import com.study.stockmanagementstudycase.base.BaseServiceTest;
+import com.study.stockmanagementstudycase.common.exception.WareHouseStockNotFoundException;
+import com.study.stockmanagementstudycase.model.WareHouseStock;
+import com.study.stockmanagementstudycase.model.entities.StockEntity;
+import com.study.stockmanagementstudycase.model.entities.WareHouseEntity;
+import com.study.stockmanagementstudycase.model.entities.WareHouseStockEntity;
+import com.study.stockmanagementstudycase.repository.WareHouseStockRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+class WareHouseStockServiceImplTest extends BaseServiceTest {
+    @InjectMocks
+    WareHouseStockServiceImpl wareHouseStockService;
+
+    @Mock
+    private WareHouseStockRepository wareHouseStockRepository;
+
+    @Test
+    void givenWareHouseStockEntities_whenGetWareHouseStocks_thenReturnWareHouseStockDomainModels() {
+        // Given
+
+        final StockEntity mockStockEntity = StockEntity.builder()
+                .id(UUID.randomUUID().toString())
+                .build();
+        final WareHouseEntity mockWareHouseEntity = WareHouseEntity.builder()
+                .id(UUID.randomUUID().toString())
+                .build();
+
+        final List<WareHouseStockEntity> mockWareHouseStockEntities = List.of(
+                WareHouseStockEntity.builder()
+                        .id(UUID.randomUUID().toString())
+                        .stockEntity(mockStockEntity)
+                        .wareHouseEntity(mockWareHouseEntity)
+                        .build(),
+
+                WareHouseStockEntity.builder()
+                        .id(UUID.randomUUID().toString())
+                        .stockEntity(mockStockEntity)
+                        .wareHouseEntity(mockWareHouseEntity)
+                        .build(),
+
+                WareHouseStockEntity.builder()
+                        .id(UUID.randomUUID().toString())
+                        .stockEntity(mockStockEntity)
+                        .wareHouseEntity(mockWareHouseEntity)
+                        .build()
+        );
+
+        // When
+        Mockito.when(wareHouseStockRepository.findAll())
+                .thenReturn(mockWareHouseStockEntities);
+
+        // Then
+        final List<WareHouseStock> response = wareHouseStockService.getWareHouseStocks();
+
+        //Assert
+        Assertions.assertNotNull(response);
+
+        Assertions.assertEquals(response.size(), 3);
+
+        //Verify
+        Mockito.verify(
+                wareHouseStockRepository,
+                Mockito.times(1)
+        ).findAll();
+    }
+
+    @Test
+    void givenEmptyWareHouseStockEntities_whenGetWareHouseStocks_thenThrowsWareHouseStockNotFoundException() {
+        // Given
+        final List<WareHouseStockEntity> mockWareHouseStockEntities = List.of();
+
+        // When
+        Mockito.when(wareHouseStockRepository.findAll())
+                .thenReturn(mockWareHouseStockEntities);
+
+        // Then
+        Assertions.assertThrowsExactly(
+                WareHouseStockNotFoundException.class,
+                () -> wareHouseStockService.getWareHouseStocks()
+        );
+
+        // Verify
+        Mockito.verify(
+                wareHouseStockRepository,
+                Mockito.times(1)
+        ).findAll();
+
+    }
+
+    @Test
+    void givenWareHouseStockEntity_whenGetWareHouseStockById_thenReturnWareHouseStockDomainModel() {
+        // Given
+        final String mockWareHouseStockId = UUID.randomUUID().toString();
+
+        final StockEntity mockStockEntity = StockEntity.builder()
+                .id(UUID.randomUUID().toString())
+                .build();
+        final WareHouseEntity mockWareHouseEntity = WareHouseEntity.builder()
+                .id(UUID.randomUUID().toString())
+                .build();
+
+        final WareHouseStockEntity mockWareHouseStockEntity = WareHouseStockEntity.builder()
+                .id(mockWareHouseStockId)
+                .stockEntity(mockStockEntity)
+                .wareHouseEntity(mockWareHouseEntity)
+                .build();
+
+        // When
+        Mockito.when(wareHouseStockRepository.findById(mockWareHouseStockId))
+                .thenReturn(Optional.of(mockWareHouseStockEntity));
+
+        // Then
+        final WareHouseStock reponse = wareHouseStockService
+                .getWareHouseStockById(mockWareHouseStockId);
+
+        Assertions.assertEquals(reponse.getId(), mockWareHouseStockId);
+
+        // Verify
+        Mockito.verify(
+                wareHouseStockRepository,
+                Mockito.times(1)
+        ).findById(mockWareHouseStockId);
+
+    }
+
+    @Test
+    void givenEmptyWareHouseStockEntity_whenGetWareHouseStockById_thenThrowsWareHouseStockNotFoundException() {
+
+        // When
+        Mockito.when(wareHouseStockRepository.findById(Mockito.anyString()))
+                .thenReturn(Optional.empty());
+
+        // Then
+        Assertions.assertThrowsExactly(
+                WareHouseStockNotFoundException.class,
+                () -> wareHouseStockService.getWareHouseStockById(Mockito.anyString())
+        );
+
+        // Verify
+        Mockito.verify(
+                wareHouseStockRepository,
+                Mockito.times(1)
+        ).findById(Mockito.anyString());
+
+    }
+
+}
