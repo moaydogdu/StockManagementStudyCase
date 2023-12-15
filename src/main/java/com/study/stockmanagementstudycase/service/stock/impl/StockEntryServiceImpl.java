8dc939a3482353c53549a022404017259f1dd4f6
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2023-12-15 14:19:27 +0100

    Refactor StockEntryServiceImpl.java

--- src/main/java/com/study/stockmanagementstudycase/service/stock/impl/StockEntryServiceImpl.java
+++ src/main/java/com/study/stockmanagementstudycase/service/stock/impl/StockEntryServiceImpl.java
@@ -1,12 +1,13 @@
 package com.study.stockmanagementstudycase.service.stock.impl;
 
 import com.study.stockmanagementstudycase.common.exception.StockNotFoundException;
-import com.study.stockmanagementstudycase.model.WareHouse;
 import com.study.stockmanagementstudycase.model.Stock;
+import com.study.stockmanagementstudycase.model.WareHouse;
+import com.study.stockmanagementstudycase.model.dto.request.stock.StockEntryRequest;
 import com.study.stockmanagementstudycase.model.entities.StockEntity;
 import com.study.stockmanagementstudycase.model.mappers.stock.StockMapper;
 import com.study.stockmanagementstudycase.repository.StockRepository;
-import com.study.stockmanagementstudycase.service.stock.StockPurchaseService;
+import com.study.stockmanagementstudycase.service.stock.StockEntryService;
 import com.study.stockmanagementstudycase.service.stockTransaction.StockTransactionCreateService;
 import com.study.stockmanagementstudycase.service.wareHouse.WareHouseService;
 import com.study.stockmanagementstudycase.service.wareHouseStock.WareHouseStockUpdateService;
@@ -19,21 +20,22 @@ import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
-public class StockPurchaseServiceImpl implements StockPurchaseService {
+public class StockEntryServiceImpl implements StockEntryService {
 
     private final StockRepository stockRepository;
     private final StockTransactionCreateService stockTransactionCreateService;
     private final WareHouseService wareHouseService;
     private final WareHouseStockUpdateService wareHouseStockUpdateService;
 
+
     @Override
-    @Transactional
-    public Stock purchaseStock(
+    public Stock entryStock(
             final String stockId,
             final String wareHouseId,
             final BigDecimal entryAmount,
             final LocalDateTime entryTime
     ) {
+
         final StockEntity stockEntityFromDbForStockPurchase = stockRepository
                 .findById(stockId)
                 .orElseThrow(StockNotFoundException::new);
@@ -41,7 +43,7 @@ public class StockPurchaseServiceImpl implements StockPurchaseService {
         final WareHouse wareHouseDomainModelForStockPurchase = wareHouseService
                 .getWareHouseById(wareHouseId);
 
-        this.updateStockAmountForPurchaseStock(
+        this.updateStockAmountForEntryStock(
                 stockEntityFromDbForStockPurchase,
                 entryAmount
         );
@@ -67,7 +69,21 @@ public class StockPurchaseServiceImpl implements StockPurchaseService {
         return stockDomainModel;
     }
 
-    private void updateStockAmountForPurchaseStock(
+    @Override
+    @Transactional
+    public Stock entryStock(
+            final String stockId,
+            final StockEntryRequest stockEntryRequest
+    ) {
+        return this.entryStock(
+                stockId,
+                stockEntryRequest.getWareHouseId(),
+                stockEntryRequest.getEntryAmount(),
+                stockEntryRequest.getEntryTime()
+        );
+    }
+
+    private void updateStockAmountForEntryStock(
             final StockEntity stockEntityForUpdateAmount,
             final BigDecimal entryAmount
     ) {
