569df17f85e588f30c637e6f993bad73c858d5d2
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2023-12-15 14:17:10 +0100

    Refactor StockEntryService.java

--- src/main/java/com/study/stockmanagementstudycase/service/stock/StockEntryService.java
+++ src/main/java/com/study/stockmanagementstudycase/service/stock/StockEntryService.java
@@ -1,15 +1,21 @@
 package com.study.stockmanagementstudycase.service.stock;
 
 import com.study.stockmanagementstudycase.model.Stock;
+import com.study.stockmanagementstudycase.model.dto.request.stock.StockEntryRequest;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public interface StockPurchaseService {
-    Stock purchaseStock(
+public interface StockEntryService {
+    Stock entryStock(
             final String stockId,
             final String wareHouseId,
             final BigDecimal entryAmount,
             final LocalDateTime entryTime
+    );
+
+    Stock entryStock(
+            final String stockId,
+            final StockEntryRequest stockEntryRequest
     );
 }
