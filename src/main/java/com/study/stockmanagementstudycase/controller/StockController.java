ea5b8723237211eb1d08d8bf24e1a0401ca33d3f
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2023-12-15 14:13:43 +0100

    Create StockController.java

--- src/main/java/com/study/stockmanagementstudycase/controller/StockController.java
+++ src/main/java/com/study/stockmanagementstudycase/controller/StockController.java
@@ -2,9 +2,11 @@ package com.study.stockmanagementstudycase.controller;
 
 import com.study.stockmanagementstudycase.model.Stock;
 import com.study.stockmanagementstudycase.model.dto.request.stock.StockCreateRequest;
+import com.study.stockmanagementstudycase.model.dto.request.stock.StockEntryRequest;
 import com.study.stockmanagementstudycase.model.dto.response.StockResponse;
 import com.study.stockmanagementstudycase.model.mappers.stock.StockDTOMapper;
 import com.study.stockmanagementstudycase.service.stock.StockCreateService;
+import com.study.stockmanagementstudycase.service.stock.StockEntryService;
 import com.study.stockmanagementstudycase.service.stock.StockService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,12 +25,23 @@ public class StockController {
 
     private final StockCreateService stockCreateService;
     private final StockService stockService;
+    private final StockEntryService stockEntryService;
 
     @PostMapping
     public ResponseEntity<Void> createStock(
             @RequestBody @Valid final StockCreateRequest stockCreateRequest
     ) {
         stockCreateService.createStock(stockCreateRequest);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{stockId}")
+    public ResponseEntity<Void> entryStock(
+            @PathVariable("stockId") @UUID final String stockId,
+            @RequestBody @Valid final StockEntryRequest stockEntryRequest
+    ) {
+        stockEntryService.entryStock(stockId, stockEntryRequest);
 
         return ResponseEntity.ok().build();
     }
