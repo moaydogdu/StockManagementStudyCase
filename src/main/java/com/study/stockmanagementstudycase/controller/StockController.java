a796f342d4f4642488f95f5b1469c7fa329bfcf0
moayd <m.o.aydogdu@outlook.com>
2023-12-21 01:22:20 +0300

    REFACTOR StockController -> Add Sale Endpoint

--- src/main/java/com/study/stockmanagementstudycase/controller/StockController.java
+++ src/main/java/com/study/stockmanagementstudycase/controller/StockController.java
@@ -3,17 +3,24 @@ package com.study.stockmanagementstudycase.controller;
 import com.study.stockmanagementstudycase.model.Stock;
 import com.study.stockmanagementstudycase.model.dto.request.stock.StockCreateRequest;
 import com.study.stockmanagementstudycase.model.dto.request.stock.StockEntryRequest;
+import com.study.stockmanagementstudycase.model.dto.request.stock.StockSaleRequest;
 import com.study.stockmanagementstudycase.model.dto.response.StockResponse;
 import com.study.stockmanagementstudycase.model.mappers.stock.StockDTOMapper;
 import com.study.stockmanagementstudycase.service.stock.StockCreateService;
 import com.study.stockmanagementstudycase.service.stock.StockEntryService;
+import com.study.stockmanagementstudycase.service.stock.StockSaleService;
 import com.study.stockmanagementstudycase.service.stock.StockService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.UUID;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -26,6 +33,7 @@ public class StockController {
     private final StockCreateService stockCreateService;
     private final StockService stockService;
     private final StockEntryService stockEntryService;
+    private final StockSaleService stockSaleService;
 
     @PostMapping
     public ResponseEntity<Void> createStock(
@@ -66,5 +74,18 @@ public class StockController {
                 .toStockResponse(stock);
 
         return ResponseEntity.ok(stockResponse);
+    }
+
+    @PostMapping("{stockId}/sale")
+    public ResponseEntity<Void> sellStock(
+            @PathVariable("stockId") @UUID final String stockId,
+            @RequestBody @Valid final StockSaleRequest stockSaleRequest
+    ) {
+        stockSaleService.saleStockById(
+                stockId,
+                stockSaleRequest
+        );
+
+        return ResponseEntity.ok().build();
     }
 }
