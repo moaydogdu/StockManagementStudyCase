bc2055494d23bb54089499b0f61395311addae98
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2023-12-26 13:49:31 +0100

    Create StockUpdateService.java

---
+++ src/main/java/com/study/stockmanagementstudycase/service/stock/StockUpdateService.java
@@ -0,0 +1,12 @@
+package com.study.stockmanagementstudycase.service.stock;
+
+import com.study.stockmanagementstudycase.model.dto.request.stock.StockUpdateRequest;
+
+public interface StockUpdateService {
+
+    void updateStock(
+            final StockUpdateRequest updateRequest,
+            final String stockId
+    );
+
+}
