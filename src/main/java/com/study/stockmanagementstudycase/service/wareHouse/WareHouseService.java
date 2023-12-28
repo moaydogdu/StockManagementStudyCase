7b9486cbf0f7b48293db8b2e84e0fba56dec2c34
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2023-12-28 01:25:50 +0100

    Refactor WareHouseService.java Changes by moayd

--- src/main/java/com/study/stockmanagementstudycase/service/wareHouse/WareHouseService.java
+++ src/main/java/com/study/stockmanagementstudycase/service/wareHouse/WareHouseService.java
@@ -4,14 +4,13 @@ import com.study.stockmanagementstudycase.common.model.dto.CustomPage;
 import com.study.stockmanagementstudycase.common.model.dto.CustomPagingRequest;
 import com.study.stockmanagementstudycase.model.WareHouse;
 
-import java.util.List;
-
 public interface WareHouseService {
 
     CustomPage<WareHouse> getWareHouses(
             final CustomPagingRequest customPagingRequest
     );
 
-    WareHouse getWareHouseById(final String wareHouseId);
-
+    WareHouse getWareHouseById(
+            final String wareHouseId
+    );
 }
