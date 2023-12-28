54810cb6b368fff49fb45feb8c7ff0736a9d66b8
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2023-12-28 01:24:49 +0100

    Refactor WareHouseServiceImpl.java

--- src/main/java/com/study/stockmanagementstudycase/service/wareHouse/impl/WareHouseServiceImpl.java
+++ src/main/java/com/study/stockmanagementstudycase/service/wareHouse/impl/WareHouseServiceImpl.java
@@ -1,12 +1,15 @@
 package com.study.stockmanagementstudycase.service.wareHouse.impl;
 
 import com.study.stockmanagementstudycase.common.exception.WareHouseNotFoundException;
+import com.study.stockmanagementstudycase.common.model.dto.CustomPage;
+import com.study.stockmanagementstudycase.common.model.dto.CustomPagingRequest;
 import com.study.stockmanagementstudycase.model.WareHouse;
 import com.study.stockmanagementstudycase.model.entities.WareHouseEntity;
 import com.study.stockmanagementstudycase.model.mappers.wareHouse.WareHouseMapper;
 import com.study.stockmanagementstudycase.repository.WareHouseRepository;
 import com.study.stockmanagementstudycase.service.wareHouse.WareHouseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,13 +22,19 @@ public class WareHouseServiceImpl implements WareHouseService {
     private final WareHouseRepository wareHouseRepository;
 
     @Override
-    public List<WareHouse> getWareHouses() {
-        final List<WareHouseEntity> wareHouseEntityList = wareHouseRepository
-                .findAll();
+    public CustomPage<WareHouse> getWareHouses(
+            CustomPagingRequest customPagingRequest
+    ) {
 
-        if (Boolean.TRUE.equals(wareHouseEntityList.isEmpty())) {
-            throw new WareHouseNotFoundException();
+        final Page<WareHouseEntity> wareHouseEntityListPage = wareHouseRepository
+                .findAll(customPagingRequest.toPageable());
+
+        if (Boolean.TRUE.equals(wareHouseEntityListPage.isEmpty())) {
+            throw new WareHouseNotFoundException("Hiç kayıtlı depo yok!");
         }
+
+        List<WareHouse> wareHouseDomainModels = WareHouseMapper
+                .toDomainModel(wareHouseEntityListPage.getContent());
 
         return wareHouseEntityList.stream()
                 .map(WareHouseMapper::toDomainModel)
