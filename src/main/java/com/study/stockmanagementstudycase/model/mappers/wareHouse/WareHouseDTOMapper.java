3f8fa57fb66b818e2a4ea465395df7ff7cb1fc40
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2023-12-28 01:22:52 +0100

    Refactor WareHouseDTOMapper.java

--- src/main/java/com/study/stockmanagementstudycase/model/mappers/wareHouse/WareHouseDTOMapper.java
+++ src/main/java/com/study/stockmanagementstudycase/model/mappers/wareHouse/WareHouseDTOMapper.java
@@ -1,10 +1,14 @@
 package com.study.stockmanagementstudycase.model.mappers.wareHouse;
 
+import com.study.stockmanagementstudycase.common.model.dto.CustomPage;
+import com.study.stockmanagementstudycase.common.model.dto.CustomPagingResponse;
 import com.study.stockmanagementstudycase.model.WareHouse;
 import com.study.stockmanagementstudycase.model.dto.response.wareHouse.WareHouseResponse;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class WareHouseDTOMapper {
 
@@ -26,11 +30,25 @@ public class WareHouseDTOMapper {
             final List<WareHouse> wareHouseList
     ) {
         if (Objects.isNull(wareHouseList)) {
-            return null;
+            return Collections.emptyList();
         }
 
         return wareHouseList.stream()
                 .map(WareHouseDTOMapper::toWareHouseResponse)
                 .toList();
+    }
+
+
+    public static CustomPagingResponse<WareHouseResponse> toPagingResponse(
+            final CustomPage<WareHouse> customPage
+    ) {
+        List<WareHouseResponse> resp = customPage.getContent().stream()
+                .map(WareHouseDTOMapper::toWareHouseResponse)
+                .toList();
+
+        return CustomPagingResponse.<WareHouseResponse>builder()
+                .content(resp)
+                .of(customPage)
+                .build();
     }
 }
