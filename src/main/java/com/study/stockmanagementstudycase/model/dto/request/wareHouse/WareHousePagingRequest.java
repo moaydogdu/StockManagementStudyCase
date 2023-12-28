9b1112ee6f5f795d222477d28d250ca0dd397d1c
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2023-12-28 01:24:19 +0100

    Create WareHousePagingRequest.java

---
+++ src/main/java/com/study/stockmanagementstudycase/model/dto/request/wareHouse/WareHousePagingRequest.java
@@ -0,0 +1,14 @@
+package com.study.stockmanagementstudycase.model.dto.request.wareHouse;
+
+import com.study.stockmanagementstudycase.common.model.dto.CustomPagingRequest;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class WareHousePagingRequest extends CustomPagingRequest {
+
+}
