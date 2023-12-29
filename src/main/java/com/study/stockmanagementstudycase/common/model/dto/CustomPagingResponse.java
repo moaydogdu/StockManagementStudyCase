642561c099cef0f08c8f2592cc4e1b915c5569f1
moayd <m.o.aydogdu@outlook.com>
2023-12-30 02:54:57 +0300

    REFACTOR CustomPagingResponse.java

--- src/main/java/com/study/stockmanagementstudycase/common/model/dto/CustomPagingResponse.java
+++ src/main/java/com/study/stockmanagementstudycase/common/model/dto/CustomPagingResponse.java
@@ -1,6 +1,5 @@
 package com.study.stockmanagementstudycase.common.model.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -20,20 +19,6 @@ public class CustomPagingResponse<T> {
 
     private Integer totalPageCount;
 
-    public CustomPagingResponse(
-            List<T> content,
-            Integer pageNumber,
-            Integer pageSize,
-            Long totalElementCount, Integer totalPageCount
-    ) {
-        this.content = content;
-        this.pageNumber = pageNumber;
-        this.pageSize = pageSize;
-        this.totalElementCount = totalElementCount;
-        this.totalPageCount = totalPageCount;
-    }
-
-
     public static class CustomPagingResponseBuilder<T> {
         public <C> CustomPagingResponse.CustomPagingResponseBuilder<T> of(
                 final CustomPage<C> customPage
@@ -43,18 +28,6 @@ public class CustomPagingResponse<T> {
                     .pageSize(customPage.getPageSize())
                     .totalElementCount(customPage.getTotalElementCount())
                     .totalPageCount(customPage.getTotalPageCount());
-        }
-
-        public CustomPagingResponse.CustomPagingResponseBuilder<T> content(
-                final List<T> _content
-        ) {
-            this.content = _content;
-            return CustomPagingResponse.<T>builder();
-        }
-
-
-        public CustomPagingResponse<T> build() {
-            return new CustomPagingResponse<T>(this.content, this.pageNumber, this.pageSize, this.totalElementCount, this.totalPageCount);
         }
     }
 }
