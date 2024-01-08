299929e00cbf6d54014f759646967d99821f2071
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-01-08 01:15:43 +0100

    Create StockResponseWithStatus

---
+++ src/main/java/com/study/stockmanagementstudycase/model/dto/response/stock/StockResponseWithStatus.java
@@ -0,0 +1,23 @@
+package com.study.stockmanagementstudycase.model.dto.response.stock;
+
+import com.study.stockmanagementstudycase.model.enums.UnitType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StockResponseWithStatus {
+    private String name;
+    private BigDecimal price;
+    private BigDecimal amount;
+    private UnitType unitType;
+    private Boolean status;
+}
