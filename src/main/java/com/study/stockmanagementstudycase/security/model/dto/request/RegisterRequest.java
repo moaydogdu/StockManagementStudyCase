ceb87bba71b6e7ca89fa2d273b031c1457424055
moayd <m.o.aydogdu@outlook.com>
2024-01-11 03:38:38 +0300

    CREATE RegisterRequest.java

---
+++ src/main/java/com/study/stockmanagementstudycase/security/model/dto/request/RegisterRequest.java
@@ -0,0 +1,17 @@
+package com.study.stockmanagementstudycase.security.model.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
