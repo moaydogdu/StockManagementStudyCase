f857177149fe4f309af24d8118b506e9356675c8
moayd <m.o.aydogdu@outlook.com>
2024-01-11 03:37:27 +0300

    CREATE AuthenticationResponse.java

---
+++ src/main/java/com/study/stockmanagementstudycase/security/model/dto/response/AuthenticationResponse.java
@@ -0,0 +1,18 @@
+package com.study.stockmanagementstudycase.security.model.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private String accessToken;
+    private String refreshToken;
+}
