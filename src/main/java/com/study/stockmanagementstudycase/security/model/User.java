2bb4dcf20f5349d60d7e3eba36d86b76f7fc20f3
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-01-21 22:24:20 +0100

    Create User.java

---
+++ src/main/java/com/study/stockmanagementstudycase/security/model/User.java
@@ -0,0 +1,29 @@
+package com.study.stockmanagementstudycase.security.model;
+
+import com.study.stockmanagementstudycase.common.model.BaseDomainModel;
+import com.study.stockmanagementstudycase.security.model.entity.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * Domain model of {@link UserEntity}
+ */
+
+@SuperBuilder
+@EqualsAndHashCode
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseDomainModel {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
