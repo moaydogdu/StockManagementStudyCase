03a8a2b5bc7aa053fdd3948c3e09690bf4eab6a8
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-01-23 22:43:10 +0100

    Refactor AuthenticationService.java, renamed getUserByUsername to getUserByEmail and AuthenticationResponse to AccessAndRefreshToken

--- src/main/java/com/study/stockmanagementstudycase/security/service/AuthenticationService.java
+++ src/main/java/com/study/stockmanagementstudycase/security/service/AuthenticationService.java
@@ -1,27 +1,16 @@
 package com.study.stockmanagementstudycase.security.service;
 
-import com.study.stockmanagementstudycase.security.exception.email.EmailNotFoundException;
-import com.study.stockmanagementstudycase.security.exception.token.TokenInterferedException;
+import com.study.stockmanagementstudycase.security.model.AccessAndRefreshToken;
 import com.study.stockmanagementstudycase.security.model.User;
 import com.study.stockmanagementstudycase.security.model.dto.request.AuthenticationRequest;
-import com.study.stockmanagementstudycase.security.model.dto.request.RegisterRequest;
 import com.study.stockmanagementstudycase.security.model.dto.response.AuthenticationResponse;
-import com.study.stockmanagementstudycase.security.model.entity.TokenEntity;
 import com.study.stockmanagementstudycase.security.model.entity.UserEntity;
-import com.study.stockmanagementstudycase.security.model.enums.TokenType;
 import com.study.stockmanagementstudycase.security.model.mappers.TokenDTOMapper;
-import com.study.stockmanagementstudycase.security.model.mappers.TokenMapper;
 import com.study.stockmanagementstudycase.security.model.mappers.UserMapper;
-import com.study.stockmanagementstudycase.security.repository.TokenRepository;
-import com.study.stockmanagementstudycase.security.repository.UserRepository;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -42,13 +31,13 @@ public class AuthenticationService {
                 )
         );
 
-        User user = userService.getUserByUsername(authenticationRequest.getEmail());
+        User user = userService.getUserByEmail(authenticationRequest.getEmail());
 
         UserEntity userEntity = UserMapper.toEntity(user);
 
-        AuthenticationResponse authenticationResponse = TokenDTOMapper
-                .toAuthenticationResponse(tokenService.generateToken(userEntity));
+        final AccessAndRefreshToken accessAndRefreshToken = tokenService
+                .generateToken(userEntity);
 
-        return authenticationResponse;
+        return TokenDTOMapper.toAuthenticationResponse(accessAndRefreshToken);
     }
 }
