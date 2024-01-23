7f1edab07ef1868bd36a1295f1d2c781e8acc8fa
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-01-23 22:45:14 +0100

    Create FilterConfig.java -> import optimization

--- src/main/java/com/study/stockmanagementstudycase/security/filter/FilterConfig.java
+++ src/main/java/com/study/stockmanagementstudycase/security/filter/FilterConfig.java
@@ -1,7 +1,5 @@
 package com.study.stockmanagementstudycase.security.filter;
 
-import com.study.stockmanagementstudycase.security.exception.user.UserNotFoundException;
-import com.study.stockmanagementstudycase.security.repository.UserRepository;
 import com.study.stockmanagementstudycase.security.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +8,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
