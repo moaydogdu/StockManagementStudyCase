665c0af727b768f681b9ac01019e56cc28af5113
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2024-02-08 02:14:49 +0300

    Create LogEntity

---
+++ src/main/java/com/study/stockmanagementstudycase/log/entity/LogEntity.java
@@ -0,0 +1,51 @@
+package com.study.stockmanagementstudycase.log.entity;
+
+import com.study.stockmanagementstudycase.common.model.entity.BaseEntity;
+import com.study.stockmanagementstudycase.security.model.entity.UserEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "LOG")
+public class LogEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "ID")
+    private String id;
+
+    @Column(name = "HTTP_METHOD")
+    private String httpMethod;
+
+    @Column(name = "PATH")
+    private String path;
+
+    @Column(name = "REQUEST_BODY")
+    private Integer status;
+
+    @Column(name = "IP")
+    private String ip;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private UserEntity user;
+}
+
