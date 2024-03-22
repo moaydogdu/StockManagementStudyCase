e9e12d54069cd66d816a05d00ed9a18c9385b2d1
moaydogdu <m.o.aydogdu@outlook.com>
2024-03-22 08:03:37 +0300

    DEV-113 CREATE CustomError.java

---
+++ src/main/java/com/study/stockmanagementstudycase/common/model/CustomError.java
@@ -0,0 +1,67 @@
+package com.study.stockmanagementstudycase.common.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+public class CustomError {
+
+    @Builder.Default
+    private LocalDateTime time = LocalDateTime.now();
+
+    private HttpStatus httpStatus;
+
+    private String header;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String message;
+
+    private Long errorCode;
+
+    @Builder.Default
+    private final Boolean isSuccess = false;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private List<SubError> subErrors;
+
+    @Getter
+    @Builder
+    public static class SubError {
+        private String message;
+        private String field;
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        private Object value;
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        private String type;
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum Header {
+
+        API_ERROR("API ERROR"),
+
+        ALREADY_EXIST("ALREADY EXIST"),
+
+        NOT_FOUND("NOT EXIST"),
+
+        VALIDATION_ERROR("VALIDATION ERROR"),
+
+        DATABASE_ERROR("DATABASE ERROR"),
+
+        PROCESS_ERROR("PROCESS ERROR"),
+
+        AUTH_ERROR("AUTH ERROR");
+
+
+        private final String name;
+    }
+
+}
