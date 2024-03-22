e9c61bf9731dbe15a63d69e24cf61fc54d786737
moaydogdu <m.o.aydogdu@outlook.com>
2024-03-22 08:05:00 +0300

    DEV-113 CREATE InvalidTokenService.java

---
+++ src/main/java/com/study/stockmanagementstudycase/auth/service/InvalidTokenService.java
@@ -0,0 +1,15 @@
+package com.study.stockmanagementstudycase.auth.service;
+
+import java.util.Set;
+
+public interface InvalidTokenService {
+
+    void invalidateTokens(
+            final Set<String> tokenIds
+    );
+
+    void checkForInvalidityOfToken(
+            final String tokenId
+    );
+
+}
