4d1d891c2a74b6b40c1d965f84b2f3f89e81f649
moaydogdu <m.o.aydogdu@outlook.com>
2024-03-22 08:15:05 +0300

    DEV-113 REFACTOR BaseEntity.java add createdBy and updatedBy fields

--- src/main/java/com/study/stockmanagementstudycase/common/model/entity/BaseEntity.java
+++ src/main/java/com/study/stockmanagementstudycase/common/model/entity/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.study.stockmanagementstudycase.common.model.entity;
 
+import com.study.stockmanagementstudycase.auth.model.enums.TokenClaims;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
@@ -9,8 +10,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 
 @Getter
@@ -24,16 +29,36 @@ public abstract class BaseEntity {
     @Column(name = "CREATED_AT")
     protected LocalDateTime createdAt;
 
-    @PrePersist
-    public void prePersist() {
-        this.createdAt = LocalDateTime.now();
-    }
+    @Column(name = "CREATED_BY")
+    protected String createdBy;
 
     @Column(name = "UPDATED_AT")
     protected LocalDateTime updatedAt;
 
+    @Column(name = "UPDATED_BY")
+    protected String updatedBy;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+
+        this.createdBy = Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                .map(Authentication::getPrincipal)
+                .filter(user -> !"anonymousUser".equals(user))
+                .map(Jwt.class::cast)
+                .map(jwt -> jwt.getClaim(TokenClaims.USER_ID.getValue()).toString())
+                .orElse("SYSTEM");
+    }
+
     @PreUpdate
     public void preUpdate() {
         this.updatedAt = LocalDateTime.now();
+
+        this.updatedBy = Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                .map(Authentication::getPrincipal)
+                .filter(user -> !"anonymousUser".equals(user))
+                .map(Jwt.class::cast)
+                .map(jwt -> jwt.getClaim(TokenClaims.USER_ID.getValue()).toString())
+                .orElse("SYSTEM");
     }
 }
